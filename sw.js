7ffae530e5893e7dce8b09d1f2bcfb46307e901f
Faraz Ardekani <farazardekani@farazs-imac.mynetworksettings.com>
2026-05-11 07:46:02 -0400

    Initial deploy

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 're-deal-calc-v4';
+const ASSETS = ['index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+// Network first, fall back to cache (ensures updates are picked up immediately)
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(response => {
+        const clone = response.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
